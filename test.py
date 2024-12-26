39a98a80ef6f1b40b22dba245a64175aade22105
linheri06 <100428580+linheri06@users.noreply.github.com>
2024-12-26 23:08:13 +0700

    Initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import torch
+from models.Transformer import Model
+from configs.one_hour.PatchTST_Configs import Configs
+
+file_path = './data_input.csv'
+data = pd.read_csv(file_path, parse_dates=['date'])
+input_series = data['soilMoisture'].values
+input_tensor = torch.tensor(input_series, dtype=torch.float32)
+input_tensor = input_tensor.view(1, -1, 1)
+print(f"Input tensor shape: {input_tensor.shape}")
+configs = Configs()
+model = Model(configs)
+model.load_state_dict(torch.load('final_model_3hour.pth', map_location=torch.device('cpu')))
+model.to(torch.device('cpu'))
+with torch.no_grad():
+    input_tensor = input_tensor.to(torch.device('cpu'))
+    output = model(input_tensor)
+print(output)
